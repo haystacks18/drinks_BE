0f2186cfb92951ad7c5f0a3db7314a7192a6171a
jonghyeon <jonghyeon9398@gmail.com>
2024-04-20 17:26:35 +0900

    Initial commit

---
+++ src/test/java/com/goormfj/hanzan/HanzanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormfj.hanzan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanzanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
